e5df03626e61ba595203761c7ef7ea1a304688f5
Mark Thiongo <mark.thiongo@student.moringaschool.com>
2024-12-08 13:10:32 +0300

    Completed lab tasks and passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+// Define the currentUser variable
+const currentUser = 'Grace Hopper';
+
+// Define the welcomeMessage variable, concatenating strings
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+
+// Define the excitedWelcomeMessage variable, converting the welcomeMessage to uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Define the shortGreeting variable, using only the first letter of currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
