a4d9b0cf76a4312cd29ce44c131c814e09b9d530
Marcglacier32 <mark.thiongo@student.moringaschool.com>
2025-01-17 15:39:10 -0800

    Completed Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+// Define the current user
+const currentUser = 'Grace Hopper'; // Replace with any name you'd like
+
+// Create the welcome message
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Create the excited welcome message
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Create the short greeting
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
